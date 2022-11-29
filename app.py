2d3056374e3009d49d376602c8c437e92aec8df4
Axewc <axew@ciencias.unam.mx>
2022-11-28 21:16:17 -0600

    Cambio de esqueleto

--- app.py
+++ app.py
@@ -1,55 +1,27 @@
-from flask import Flask, render_template, redirect, url_for, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, redirect, url_for
+from alchemyClasses.usuario import db
+from controllers.auth import bp
+from controllers.login import loginBlueprint
+from controllers.logout import logoutBlueprint
+from controllers.reservations import view_reservations
 
+app = Flask(__name__, instance_relative_config=True)
+app.register_blueprint(bp)
+app.register_blueprint(loginBlueprint)
+app.register_blueprint(logoutBlueprint)
+app.register_blueprint(view_reservations)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:210622@localhost:3306/prueba"
+app.config.from_mapping(
+    SECRET_KEY = 'dev'
+)
+db.init_app(app)
 
-#Configuraciones
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost:3306/prueba"
-db = SQLAlchemy(app)
-
-#Mapeo al ORM
-class Usuario(db.Model):
-
-    email = db.Column('correo', db.String(200), primary_key=True)
-    nombre = db.Column('nombre', db.String(200))
-    passwd = db.Column('contrasena', db.String(40))
-
-    def __init__(self, email, nombre, passwd):
-        self.email = email
-        self.nombre = nombre
-        self.passwd = passwd
-
-
-#Consulta
-def obtenUsuario(email_arg):
-    ans = Usuario.query.filter(Usuario.email==email_arg).first()
-    return ans
-
-#Controller
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        username = request.form['username']
-        passwd = request.form['password']
-        email = request.form['email']
-        usuario = Usuario(email, passwd, username)
-        if obtenUsuario(email) != None:
-            return render_template('success.html')
-        else:
-            return render_template('fail.html')
-        """
-        Como ya se hizo el request.form, ya se consumio el valor esperado de la vista, entonces esto causa error.
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:"""
-        return redirect(url_for('home'))
-    return render_template('/home/fergbb/Documentos/Semestre7/Software/Proyecto/Bersama-IS/templates/html/login.html', error=error)
-
-@app.route("/", methods=['GET'])
-def home():
-    return "<p>Hello world!</p>"
+@app.route('/', methods=['GET','POST'])
+def hello_world():
+    return redirect(url_for('login.login'))
 
 if __name__ == '__main__':
     db.create_all()
     app.run()
+
+
