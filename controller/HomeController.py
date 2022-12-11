92ef8582778c14618c49e093325689ad5ae9b401
Fergb01 <fer_gb@ciencias.unam.mx>
2022-12-10 22:59:42 -0600

    ya se ven las imagenes

--- controller/HomeController.py
+++ controller/HomeController.py
@@ -9,3 +9,11 @@ home = Blueprint('home', __name__)
 def show():
     return render_template('index.html')
 
+
+@home.route('/registro.html', methods=['GET', 'POST'])
+def registro():
+    return render_template('registro.html')
+
+@home.route("/index.html", methods=['GET', 'POST'])
+def showd():
+    return render_template('index.html')
